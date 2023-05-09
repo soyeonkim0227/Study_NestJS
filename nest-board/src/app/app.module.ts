f5499359e40b3028ecefe09aea324c17315eab5b
soyeonkim0227 <soyeonkim0227@naver.com>
2023-05-09 14:18:03 +0900

    UPDATE nest-board app module

--- nest-board/src/app/app.module.ts
+++ nest-board/src/app/app.module.ts
@@ -1,12 +1,13 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { BoardModule } from 'src/board/board.module';
+import { UserModule } from 'src/user/user.module';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import connectionOptions from './ormconfig';
 
 @Module({
-  imports: [TypeOrmModule.forRoot(connectionOptions), BoardModule],
+  imports: [TypeOrmModule.forRoot(connectionOptions), BoardModule, UserModule],
   controllers: [AppController],
   providers: [AppService],
 })

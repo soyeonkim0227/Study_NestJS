39dfc9f718a86c8d80490452de85b035c83f8dcc
soyeonkim0227 <soyeonkim0227@naver.com>
2023-05-19 23:50:03 +0900

    CREATE nest-passport user entity

---
+++ nest-passport/src/user/user.entity.ts
@@ -0,0 +1,16 @@
+import { Column, Entity, PrimaryColumn, PrimaryGeneratedColumn } from 'typeorm';
+
+@Entity()
+export class User {
+  @PrimaryGeneratedColumn()
+  user_id: number;
+
+  @Column()
+  email: string;
+
+  @Column()
+  username: string;
+
+  @Column()
+  password: string;
+}

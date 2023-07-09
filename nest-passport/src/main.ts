11c47435235b3741dc3f66248ac0d39bf5eb5c5d
soyeonkim0227 <soyeonkim0227@naver.com>
2023-07-09 16:17:29 +0900

    CREATE nest-passport applying exception filter

--- nest-passport/src/main.ts
+++ nest-passport/src/main.ts
@@ -1,8 +1,14 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app/app.module';
+import { HttpExceptionFilter } from './filter/http-exception.filter';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  app.enableCors();
+
+  app.useGlobalFilters(new HttpExceptionFilter());
+
   await app.listen(3000);
 }
 bootstrap();
